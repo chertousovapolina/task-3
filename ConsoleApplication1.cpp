72976b9536601a37281394989341c42527dd9690
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-13 21:21:28 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a;
+    float b;
+    float c;
+    float D;
+    cin >> a >> b >> c;
+           D = b * b - 4 * a * c;
+    
+       
+
+
+    cout << D;
+    
+}
